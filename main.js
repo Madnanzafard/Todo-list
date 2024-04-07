451bdd1df5bbc9913926fbf022e4ff9587a907a8
Muhammad Adnan Zafar <160720380+Madnanzafard@users.noreply.github.com>
2024-04-07 14:50:22 +0500

    Commit todo list

---
+++ main.js
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    { name: "pin",
+        message: "Enter Your pin number",
+        type: "number",
+    }
+]);
+let todoLength = await inquirer.prompt([
+    {
+        name: "length",
+        meassage: "enter the length of todo list",
+        tpye: "number",
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    while (condition) {
+        let addTask = await inquirer.prompt([
+            {
+                name: "todo",
+                message: "what you want to add in your todo list",
+                type: "input",
+            },
+            {
+                name: "addMore",
+                message: "Do you want to add more ?",
+                type: "confirm",
+                defualt: "false",
+            }
+        ]);
+        todos.push(addTask.todo);
+        if (todos.length >= todoLength.length) {
+            console.log("Maximum todo list length reached");
+            break;
+        }
+        condition = addTask.addMore;
+        console.log(todos);
+    }
+    ;
+}
+else {
+    console.log("your pin is incorrect");
+}
+;
